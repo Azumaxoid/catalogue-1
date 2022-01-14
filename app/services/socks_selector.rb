b941a2939b05758843bf9b633c1d58fe6f898c41
tfuru-nr <tfurugaki@newrelic.com>
2022-01-14 11:15:28 +0900

    split tags

--- app/services/socks_selector.rb
+++ app/services/socks_selector.rb
@@ -10,9 +10,10 @@ class SocksSelector
 
   def call
     @socks = [];
+    @tags = @tags.split(",")
     if @tags.length > 0
       @socks = Sock.all.filter {
-        |sock| @tags.find { |tag| tag == Tag.find(SockTag.find_by(sock_id: sock.sock_id).tag_id).name }
+        |sock| @tags.find { |tag| tag == Tag.find(SockTag.find_by(sock_id: sock.sock_id).tag_id).name}
       }
     else
       @socks = Sock.all
