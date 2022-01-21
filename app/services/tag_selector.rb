640759dd167e2b257a62d602d88d0cdce19c9c34
csk-nr <southern_seaane@hotmail.com>
2022-01-21 14:59:32 +0900

    modigy tag selector

--- app/services/tag_selector.rb
+++ app/services/tag_selector.rb
@@ -1,17 +1,16 @@
 class TagSelector
   include Service
 
-  def call
-    tags = Tag.all.map{|tag| tag.name}
-    count = tags.size
+  def index
+    @tags = Tag.all.map{|tag| tag.name}
     # Green socks tag have a bug so filtered
-    tags = tags.filter {|item| item != "green"}
-    result = []
-    for @i in 0..count-1
-      result.append(tags.fetch(@i))
+    @tags = @tags.filter {|item| item != "green"}
+    @connt = @tags.size
+    @newTags = []
+    for @i in 0..@count-1
+      @newTags.append(@tags.fetch(@i))
     end
-    result = result.sort{|a, b| a <=> b }
-    return result
+    render json: { "tags": @newTags }
   end
 
 end
